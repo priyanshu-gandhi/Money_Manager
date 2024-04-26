59b189ae50628d3e85fabf89c3480ddb59e8ecbe
Priyanshu Gandhi <gandhipriyanshu1942@gmail.com>
2024-04-26 15:39:20 +0530

    First Commit
    A simple money manager lets you record your income and expenses to track your finances. You can add details about each transaction, such as the amount, date, and category (e.g., rent, groceries, salary). The tool then calculates your current balance, letting you see where your money goes and how much you have left. It's like a digital ledger, helping you budget and manage your funds effectively.

---
+++ chart.js
@@ -0,0 +1,40 @@
+//select chart element
+
+const chart= document.querySelector(".chart");
+
+//create canvas element
+const canvas=document.createElement("canvas");
+canvas.width=50;
+canvas.height=50;
+
+//append canvas to chart element
+chart.appendChild(canvas);
+
+//to draw on canvas , we need to get context of canvas
+const ctx=canvas.getContext("2d");
+
+//change the line width
+ctx.lineWidth=8;
+
+
+//circle radius
+const R=20;
+
+function drawCircle(color, ratio, anticlockwise)
+{
+    ctx.strokeStyle =color;
+    ctx.beginPath();
+    ctx.arc(canvas.width/2 ,canvas.height/2, R,0, ratio *2* Math.PI ,anticlockwise);
+    ctx.stroke();
+}
+
+function updateChart(income, outcome)
+{
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    let ratio= income/ (income+outcome);
+
+    drawCircle("#FFFFFF", -ratio, true);
+    drawCircle("#F0624D",1-ratio, false);
+}
+
+
